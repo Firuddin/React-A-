5a8bbe51e85d03e901b0cb6147582dd7694aa2f6
Firuddin <firuddinaliyev888@gmail.com>
2024-08-11 16:20:10 +0400

    add

--- React-telim/src/App.jsx
+++ React-telim/src/App.jsx
@@ -3,16 +3,32 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+// function App() {
+//   return < Child name = "ALi" age={25}/>
+// }
+
+// function Child(props) {
+//   return(
+//    <div>
+//     <p>ad:{props.name}</p>
+//     <p>yas:{props.age}</p>
+//    </div>
+//   )
+// }
+// export default App
+
+
 function App() {
-  return <Child name = "ELi" age= {25}/>
+  return < Child name="Ali" age = {25}/>
 }
 
-function Child(props) {
+function Child({name,age}) {
   return(
     <div>
-      <p>ad:{props.name}</p>
-      <p>age:{props.age}</p>
+      <p>ad:{name}</p>
+      <p>age:{age}</p>
     </div>
   )
 }
+
 export default App
